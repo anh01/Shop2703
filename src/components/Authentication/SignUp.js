78c2648bc93794f613087b33369c6f4281a3a55a
Van Pho Nguyen <vanpho02@gmail.com>
2017-06-02 19:00:18 +0700

    sign up

--- src/components/Authentication/SignUp.js
+++ src/components/Authentication/SignUp.js
@@ -1,17 +1,60 @@
 import React, { Component } from 'react';
 import { View, TextInput, TouchableOpacity, Text, StyleSheet } from 'react-native';
-
+import signUpAPI from '../../api/signUp';
 
 export default class SignUp extends Component {
+
+    constructor(props) {
+        super(props);
+        this.state = {
+            email: '',
+            password: '',
+            repass: '',
+            name: ''
+        };
+    }
+
+    onSignUp() {
+        const { email, password, name } = this.state;
+        signUpAPI(email, password, name)
+            .then(a => console.log(a));
+    }
+
     render() {
         const { inputStyle, bigButton, buttonText } = styles;
+        const { email, password, repass, name } = this.state;
         return (
             <View>
-                <TextInput style={inputStyle} placeholder="Enter your name" />
-                <TextInput style={inputStyle} placeholder="Enter your email" />
-                <TextInput style={inputStyle} placeholder="Enter your password" />
-                <TextInput style={inputStyle} placeholder="Re-enter your password" />
-                <TouchableOpacity style={bigButton}>
+                <TextInput
+                    style={inputStyle}
+                    placeholder="Enter your name"
+                    autoCapitalize="none"
+                    value={name}
+                    onChangeText={text => this.setState({ name: text })}
+                />
+                <TextInput
+                    style={inputStyle}
+                    placeholder="Enter your email"
+                    autoCapitalize="none"
+                    value={email}
+                    onChangeText={text => this.setState({ email: text })}
+                />
+                <TextInput
+                    style={inputStyle}
+                    placeholder="Enter your password"
+                    autoCapitalize="none"
+                    secureTextEntry
+                    value={password}
+                    onChangeText={text => this.setState({ password: text })}
+                />
+                <TextInput
+                    style={inputStyle}
+                    placeholder="Re-enter your password"
+                    autoCapitalize="none"
+                    value={repass}
+                    onChangeText={text => this.setState({ repass: text })}
+                />
+                <TouchableOpacity style={bigButton} onPress={this.onSignUp.bind(this)}>
                     <Text style={buttonText}>SIGN UP NOW</Text>
                 </TouchableOpacity>
             </View>
@@ -42,6 +85,3 @@ const styles = StyleSheet.create({
         fontWeight: '400'
     }
 });
-
-
-
